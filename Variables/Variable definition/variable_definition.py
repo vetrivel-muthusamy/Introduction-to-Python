abb5eb174f2f9be805243fe2e23960fcc5652c3d
vetrivel-muthusamy <vetrivelm02@gmail.com>
2024-02-25 11:03:12 +0530

    This Is an initial commit

---
+++ Variables/Variable definition/variable_definition.py
@@ -0,0 +1,19 @@
+a = 1
+# We'll explain the expression str(a) later in the course.
+# For now: it is used to convert the variable "a" into a string.
+print("a = " + str(a))
+
+# Assign "World" to the name variable using the assignment operator
+name = "World"
+print("Hello, " + str(name))
+# Reassign the name variable with your actual name
+name = "Vetrivel" # Assign a new value
+print("Hello, " + str(name))
+
+
+# Use chain assignment here to assign the value 2 to variables "a" and "b".
+a = 2
+b = 2
+# Assign 2 to both a and b here 2
+print("a = " + str(a))
+print("b = " + str(b))
